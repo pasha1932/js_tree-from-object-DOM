e70f9c40554107d9c25ef47e902b2c40b611bcb3
Pavlo Pukalo <misterkatlet@gmail.com>
2024-09-06 12:44:05 +0300

    make build

---
+++ index.fd11e433.js
@@ -0,0 +1,2 @@
+!function(e,n){e.append(function e(n){if(Object.keys(n).length){var r=document.createElement("ul");for(var t in n){var a=document.createElement("li");a.innerHTML=t;var c=e(n[t]);c&&a.append(c),r.append(a)}return r}}(n))}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.fd11e433.js.map
